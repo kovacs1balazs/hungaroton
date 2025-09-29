627f3cf5ae95aabed1c56a8cf405943da3bcdf0d
kovacs1balazs <kovacs1balazs@gmail.com>
2025-09-29 11:10:24 +0200

    feat: init setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+export default function Page() {
+  return (
+    <Container sx={{ py: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Hungaroton Artists
+      </Typography>
+    </Container>
+  );
+}
