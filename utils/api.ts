4f8c4a9c13b492cc2f206924a70248015ac3b6c7
kovacs1balazs <kovacs1balazs@gmail.com>
2025-09-29 12:40:07 +0200

    fix: pagination, letter

--- utils/api.ts
+++ utils/api.ts
@@ -1,32 +1,46 @@
 export interface Artist  {
-  id: string;
+  id: number;
   name: string;
   albumCount: number;
   portrait?: string;
 };
 
-export async function fetchArtists(params: {
+export interface Pagination {
+
+  current_page: number,
+  total_pages: number,
+  per_page: number,
+  total_items: number;
+}
+
+
+export type FetchArtistsParams = {
   page?: number;
-  search?: string;
   per_page?: number;
-}) {
+  search?: string;
+  letter?: string;
+  type?: "is_composer" | "is_performer" | "is_primary";
+};
+
+export type FetchArtistsResponse = {
+  data: Artist[];
+  pagination: Pagination;
+};
+
+export async function fetchArtists(params: FetchArtistsParams): Promise<FetchArtistsResponse> {
   const query = new URLSearchParams({
     include_image: "true",
-    per_page: (params.per_page || 50).toString(),
     page: (params.page || 1).toString(),
+    per_page: (params.per_page || 50).toString(),
   });
 
-  if (params.search) {
-    query.set("search", params.search);
-  }
+  if (params.search) query.set("search", params.search);
+  if (params.letter) query.set("letter", params.letter);
+  if (params.type) query.set("type", params.type);
 
-  const res = await fetch(`https://exam.api.fotex.net/api/artists?${query}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(`https://exam.api.fotex.net/api/artists?${query.toString()}`, { cache: "no-store" });
 
-  if (!res.ok) {
-    throw new Error("API error");
-  }
+  if (!res.ok) throw new Error("API error");
 
   return res.json();
 }
