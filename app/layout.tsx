926dbcbe27fc8c3c1b581e68083bead29d91a86c
kovacs1balazs <kovacs1balazs@gmail.com>
2025-09-29 12:47:49 +0200

    fix: background, font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
+  weight: ["300","400","500","700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
+  weight: ["400","700"],
 });
 
 export const metadata: Metadata = {
   title: "Hungaroton Artists",
-  description: "",
+  description: "Artist list with modern UI",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        style={{ backgroundColor: "#e0eae0" }}
+        className={`${roboto.variable} ${robotoMono.variable}`}
+      >
         {children}
       </body>
     </html>
